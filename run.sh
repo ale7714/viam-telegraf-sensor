3d1c70cdb3d382e92eb4754319c49f4b2d726711
Ale Paredes <aleu@viam.com>
2024-02-13 14:24:21 -0500

    fix install step

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ if [[ $(command -v $SUDO) == "" ]]; then
     echo "No sudo on this system, Telegraf sensor must run with sudo access."
 else 
     if [[ $(command -v telegraf) == "" ]]; then
-        if [ "$OS" == "Darwin" ]; then
+        if [ "$OS" == "darwin" ]; then
             if [[ $(command -v brew) == "" ]]; then
                 echo "Unable to install Telegraf due to Homebrew missing in the system."
                 echo "Please, install Hombrew before running the sensor."
@@ -17,10 +17,9 @@ else
                 brew update && brew install telegraf
             fi        
         fi
-        if [ "$OS" == "Linux" ]; then
+        if [ "$OS" == "linux" ]; then
 
                 if command -v apt-get; then
-                    # influxdata-archive_compat.key GPG Fingerprint: 9D539D90D3328DC7D6C8D3B9D8FF8E1F7DF8B07E
                     curl -s https://repos.influxdata.com/influxdata-archive_compat.key > influxdata-archive_compat.key
                     echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | $SUDO tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
                     echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | $SUDO tee /etc/apt/sources.list.d/influxdata.list
