1fabd0a4788939315183d59d7b5a9fde3c362734
Ale Paredes <1709578+ale7714@users.noreply.github.com>
2024-03-05 13:46:02 -0500

    Update run.sh to check for sudo no interactive and only when necessary
    Signed-off-by: Ale Paredes <1709578+ale7714@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -5,35 +5,36 @@
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 SUDO=sudo
 
-if [[ $(command -v $SUDO) == "" ]]; then
-    echo "No sudo on this system, Telegraf sensor must run with sudo access."
-else 
-    if [[ $(command -v telegraf) == "" ]]; then
+if [[ $(command -v telegraf) == "" ]]; then
         if [ "$OS" == "darwin" ]; then
             if [[ $(command -v brew) == "" ]]; then
                 echo "Unable to install Telegraf due to Homebrew missing in the system."
-                echo "Please, install Hombrew before running the sensor."
-            else
+                echo "Please, install Homebrew before running the sensor."
+            else 
                 brew update && brew install telegraf
             fi        
-        fi
-        if [ "$OS" == "linux" ]; then
-
-                if command -v apt-get; then
+        elif [ "$OS" == "linux" ]; then
+            if command -v apt-get; then
+                if $SUDO -n true 2>&1; then
                     curl -s https://repos.influxdata.com/influxdata-archive_compat.key > influxdata-archive_compat.key
                     echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | $SUDO tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
                     echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | $SUDO tee /etc/apt/sources.list.d/influxdata.list
                     $SUDO apt-get update && $SUDO apt-get install telegraf
-                else
-                    echo "Unable to install Telegraf due to apt-get missing in the system."
-                    echo "Please, install Telgraf on your device following your system specific instructions:"
-                    echo "https://docs.influxdata.com/telegraf/v1/install/"
+                else    
+                    echo "Telegraf sensor must run with sudo permission. Run viam-server with sudo."
+                    exit 1
                 fi
+            else
+                echo "Unable to install Telegraf due to apt-get missing in the system."
+                echo "Please, install Telegraf on your device following your system specific instructions:"
+                echo "https://docs.influxdata.com/telegraf/v1/install/"
+            fi
         fi
-    else 
-        echo "Telegraf avialable in the system. Skipping installation."
-    fi
-    $SUDO cp viam-telegraf.conf /etc/viam-telegraf.conf
 
-    exec ./bin "$@"
+else 
+    echo "Telegraf available in the system. Skipping installation."
 fi
+
+cp viam-telegraf.conf /tmp/viam-telegraf.conf
+
+exec ./bin "$@"
